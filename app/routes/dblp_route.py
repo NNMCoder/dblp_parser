092c9f06ee87c3ad3e2926cf5c9168ba33893fd9
Nikita Nosov <nnmastodont@gmail.com>
2022-10-19 13:06:47 +0300

    Fix added data in DB

--- app/routes/dblp_route.py
+++ app/routes/dblp_route.py
@@ -41,8 +41,9 @@ async def get_author_subject_area(query: BaseQuery):
 
     if not results:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Authors not found")
-    for result in results[0]:
-        update_author_info_db((randint(1,9999),
+    for result_one in results:
+        for result in result_one:
+            update_author_info_db((randint(1,9999),
                                '',
                                None,
                                result[0].get('cited_count'),
